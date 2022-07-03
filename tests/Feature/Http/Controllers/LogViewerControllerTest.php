ff61390fe9e679444fea76c470c71b609a6287b1
marchampson <marchampson@users.noreply.github.com>
2022-07-03 19:30:50 +0000

    Fix styling

--- tests/Feature/Http/Controllers/LogViewerControllerTest.php
+++ tests/Feature/Http/Controllers/LogViewerControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 //
 //namespace VendorName\Skeleton\Tests\Feature\Http\Controllers;
 //
@@ -8,8 +9,8 @@
 //}
 use Marchampson\LaravelKonamiLogViewer\Http\Controllers\LogViewerController;
 
-it('has a route to the log viewer', function() {
-   $this
+it('has a route to the log viewer', function () {
+    $this
        ->get(action([LogViewerController::class, 'index']))
        ->assertOk()
        ->assertSee('ok');
