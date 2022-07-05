deaf0bc1d0d7a0bd562f8ce239b928e77b1f4e20
Marc Hampson <mhampson@wk360.com>
2022-07-05 17:12:47 +0100

    Swapping tests around to see if file available for second test

--- tests/Feature/Http/Controllers/LogViewerControllerTest.php
+++ tests/Feature/Http/Controllers/LogViewerControllerTest.php
@@ -2,13 +2,13 @@
 
 use Marchampson\LaravelKonamiLogViewer\Http\Controllers\LogViewerController;
 
-it('has a log file to view', function () {
-    $this->assertFileExists(storage_path('logs/laravel.log'));
-});
-
 it('has a route to the log viewer', function () {
     $this
        ->get(action([LogViewerController::class, 'index']))
        ->assertOk()
        ->assertSee('laravel.log');
+});
+
+it('has a log file to view', function () {
+    $this->assertFileExists(storage_path('logs/laravel.log'));
 });
